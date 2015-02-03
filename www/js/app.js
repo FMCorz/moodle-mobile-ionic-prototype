314e99f0939f2b82290f33aa4f5f83a1226cd41c
dpaloucva <dpalou@macbook-13-de-laia-canet.local>
2015-02-03 13:40:18 +0100

    Report bug now links to Moodle tracker

--- www/js/app.js
+++ www/js/app.js
@@ -515,6 +515,19 @@ angular.module('mm', [
       }
     })
 
+    .state('site.settings-report', {
+      tablet: {
+        parent: 'site.settings',
+        node: 'report'
+      },
+      url: '/report',
+      views: {
+        'site': {
+          templateUrl: 'tpl/site-settings-report.html'
+        }
+      }
+    })
+
     .state('site.settings-about', {
       tablet: {
         parent: 'site.settings',
